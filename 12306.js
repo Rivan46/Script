17e91aa143403a6fdfeb6b444ff4ee597cf82bc3
Rivan46 <114002926+Rivan46@users.noreply.github.com>
2022-12-01 09:02:51 +0800

    Update 12306.js

--- 12306.js
+++ 12306.js
@@ -1,5 +1,7 @@
+/*
 
-12306 去除倒计时 2022-11-30
+
+12306 去除倒计时 made by ddgksf2013 on 2022-11-30
 
 请检查你的分流是否有ad.12306.cn，如果有，需要去除（可使用解析器排出#out=12306），不然重写是无法生效的，同时检查是否有与其冲突的重写
 
@@ -13,4 +15,38 @@ hostname = ad.12306.cn
 
 
 
-var Rivan=JSON.parse($response.body);Rivan.materialsList&&(1==Rivan.materialsList.length?(Rivan.materialsList[0].filePath="",Rivan.advertParam.skipTime=1,Rivan.advertParam.skipTimeAgain=5,Rivan.advertParam.showSkipBtn=-1):1<Rivan.materialsList.length&&(Rivan.materialsList=[{}])),$done({body:JSON.stringify(Rivan)});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var ddgksf2013=JSON.parse($response.body);ddgksf2013.materialsList&&(1==ddgksf2013.materialsList.length?(ddgksf2013.materialsList[0].filePath="",ddgksf2013.advertParam.skipTime=1,ddgksf2013.advertParam.skipTimeAgain=5,ddgksf2013.advertParam.showSkipBtn=-1):1<ddgksf2013.materialsList.length&&(ddgksf2013.materialsList=[{}])),$done({body:JSON.stringify(ddgksf2013)});
+
